de0d68314e08c4771ee642ad7d59ec8c25cc4b23
andre <andre@192.168.2.35>
2020-08-05 14:07:11 -0400

    (#7) Finished Algebra Engine (Functional)

--- src/com/andrew/phys/equ/Algebra.java
+++ src/com/andrew/phys/equ/Algebra.java
@@ -1,14 +1,15 @@
 package com.andrew.phys.equ;
 
-import java.util.Scanner;
-
+/**
+ * This class handles the algebra behind
+ * @author andre
+ *
+ */
 public class Algebra {
-
-	// DEBUGGING
-	public static Scanner in = new Scanner(System.in);
 	
 	/**
-	 * Rearranges an equation to solve for a variable
+	 * Rearranges an equation to solve for a variable. However, the equation</br>
+	 * will be in its raw form (No simplification)
 	 * 
 	 * @param org
 	 * The original equation
@@ -18,18 +19,15 @@ public class Algebra {
 	 * @return
 	 * The rearranged equation
 	 */
-	public static String rearrange(String org, String solve) {
+	private static String rearrangeRaw(String org, String solve) {
 		// Divides equation into left side and right side
 		String[] sides = org.split("=");
-		// Remove Brackets if Needed
-		sides[0] = (sides[0].matches("\\([^\\)]+\\)"))? sides[0].replaceAll("\\((?<in>[^\\)]+)\\)", "${in}") : sides[0];
-		sides[1] = (sides[1].matches("\\([^\\)]+\\)"))? sides[1].replaceAll("\\((?<in>[^\\)]+)\\)", "${in}") : sides[1];
-		// Equ
-		System.out.println("BEGIN >> " + sides[0] + "=" + sides[1]);
-		in.nextLine();
 		// If already rearranged, return
 		if(sides[0].equals(solve) && !sides[1].contains(solve))
 			return org;
+		// Remove Brackets if Needed
+		sides[0] = (sides[0].matches("\\([^\\)]+\\)"))? sides[0].replaceAll("\\((?<in>[^\\)]+)\\)", "${in}") : sides[0];
+		sides[1] = (sides[1].matches("\\([^\\)]+\\)"))? sides[1].replaceAll("\\((?<in>[^\\)]+)\\)", "${in}") : sides[1];
 		// Ensures the desired variable is on the left side
 		if(sides[1].contains(solve) && !sides[0].contains(solve))
 			return rearrange(sides[1] + "=" + sides[0], solve);
@@ -53,15 +51,12 @@ public class Algebra {
 					nwLS += ((nwLS.equals(""))? "" : "+") + t;
 				// Move to Right Side
 				else
-					sides[1] += "+-" + t;
+					sides[1] += "-" + t;
 			}
 			// Update LS
 			sides[0] = nwLS;
 		}
-		// Equ
-		System.out.println("ADD >> " + sides[0] + "=" + sides[1]);
 		// Multiple and Divide
-		// TODO it doesnt
 		if(sides[0].contains("*")) {
 			// Split into terms
 			String[] terms = sides[0].split("\\*");
@@ -81,29 +76,69 @@ public class Algebra {
 					nwLS += ((nwLS.equals(""))? "" : "*") + t;
 				// Move to Right Side
 				else
-					sides[1] = "(" + sides[1] + ")*" + t + "^-1";
+					sides[1] = "(" + sides[1] + ")/" + t + "";
 			}
 			// Update LS
 			sides[0] = nwLS;
 		}
-		// Equ
-		System.out.println("MULT >> " + sides[0] + "=" + sides[1]);
-		// Simplify
-		String fin = sides[0] + "=" + sides[1];
-		// Removal Extra Signs
-		while(fin.contains("--") || fin.contains("++") || fin.contains("+-") || fin.contains("-+")) {
-			fin = fin.replaceAll("\\+{2}|\\-{2}", "+");
-			fin = fin.replaceAll("\\+\\-|\\-\\+", "-");
+		// Exponent
+		if(sides[0].contains("^")) {
+			// Split into terms
+			String[] terms = sides[0].split("\\^");
+			// Rearrange for the term that contains {solve}
+			String nwLS = "";
+			int bracket = 0;
+			for(String t : terms) {
+				// Detected Bracket
+				if(t.contains("("))
+					bracket++;
+				if(t.contains(")"))
+					bracket--;
+				// Ignore Term if it has the {solve}
+				// Ignore if within bracket
+				if(t.contains(solve) || bracket > 0) 
+					// Ensures no * at beginning
+					nwLS += ((nwLS.equals(""))? "" : "^") + t;
+				// Move to Right Side
+				else
+					sides[1] = t + "√(" + sides[1] + ")";
+			}
+			// Update LS
+			sides[0] = nwLS;
 		}
-		// Equ
-		System.out.println("END >> " + sides[0] + "=" + sides[1]);
+		// Reprocess
+		return rearrange(sides[0] + "=" + sides[1], solve);
+	}
+	
+	/**
+	 * Rearranges an equation to solve for a variable
+	 * 
+	 * <p>
+	 * This does not support log, trig func or factoring yet
+	 * </p>
+	 * 
+	 * @param org
+	 * The original equation
+	 * @param solve
+	 * The variable to solve for
+	 * 
+	 * @return
+	 * The rearranged equation
+	 */
+	public static String rearrange(String org, String solve) {
+		// Get Rearranged String
+		String fin = rearrangeRaw(org, solve);
+		// Replace +-, etc
+		while(fin.contains("+-") || fin.contains("-+") || fin.contains("--") || fin.contains("++")) {
+			fin = fin.replaceAll("\\+{2}|-{2}", "+");
+			fin = fin.replaceAll("\\+-|-\\+", "-");
+		}
 		// Return
-		return rearrange(fin, solve);
+		return fin;
 	}
 	
 	public static void main(String... strings) {
-		// TODO Figure out a way to deal with brackets
-		System.out.println(rearrange("y=2*(5*x+-6*g)+2*a", "g"));
+		System.out.println(rearrange("s=u*t+0.5*a*t^2", "a"));
 	}
 	
 }
